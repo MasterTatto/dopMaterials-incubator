09671ffc6df48e61a96c38a55046f9c8cefb7a01
Alex <74793187+MasterTatto@users.noreply.github.com>
2021-07-06 23:34:12 +0300

    add reducer and test

--- src/components/Accordion/UncontrolledAccordion.tsx
+++ src/components/Accordion/UncontrolledAccordion.tsx
@@ -1,41 +1,45 @@
-import React, { useState } from 'react';
+import React, {useReducer} from "react";
+import {reducer} from "./Reducer";
 
 type AccordionPropsType = {
-	titleValue: string;
-	collapsed: boolean;
+    titleValue: string;
+    collapsed: boolean;
 };
 
 export function UncontrolledAccordion(props: AccordionPropsType) {
-	const [value, setValue] = useState(props.collapsed);
-	console.log('Accordion rendering');
-	return (
-		<div>
-			<AccordionTitle
-				title={props.titleValue}
-				onClick={() => setValue(!value)}
-			/>
-			{!value && <AccordionBody />}
-		</div>
-	);
+    // const [collapsed, setCollapsed] = useState(false);
+    const [state, dispatch] = useReducer(reducer, {collapsed: false});
+    console.log("Accordion rendering");
+    return (
+        <div>
+            <AccordionTitle
+                title={props.titleValue}
+                onClick={() => {
+                    dispatch({type: "TOGGLE-COLLAPSED"});
+                }}
+            />
+            {!state.collapsed && <AccordionBody/>}
+        </div>
+    );
 }
 
 type AccordionTitlePropsType = {
-	title: string;
-	onClick: () => void;
+    title: string;
+    onClick: () => void;
 };
 
 function AccordionTitle(props: AccordionTitlePropsType) {
-	console.log('AccordionTitle rendering');
-	return <h3 onClick={() => props.onClick()}>---{props.title}---</h3>;
+    console.log("AccordionTitle rendering");
+    return <h3 onClick={() => props.onClick()}>---{props.title}---</h3>;
 }
 
 function AccordionBody() {
-	console.log('AccordionBody rendering');
-	return (
-		<ul>
-			<li>1</li>
-			<li>2</li>
-			<li>3</li>
-		</ul>
-	);
+    console.log("AccordionBody rendering");
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    );
 }
